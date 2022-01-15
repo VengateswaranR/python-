0ac8e30566886e43971ba1d3f525dbd72af95d75
VengateswaranR <vengateswaranr6@gmail.com>
2022-01-15 13:13:22 +0530

    K symbol

---
+++ structure16.py
@@ -0,0 +1,9 @@
+n=int(input())
+for i in range(0,n):                         
+    for j in range(0,n):              
+        if(i==j and i>=n//2 or j==n-1-i and j>=n//2 or j==n//2):                
+            print(j,end=' ')         
+        else:            
+            print(end='  ')          
+    print()
+
