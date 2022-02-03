da01bb03d72d3c6432c99bd2f95c729bc11075d2
VengateswaranR <97583836+VengateswaranR@users.noreply.github.com>
2022-02-03 13:51:33 +0530

    Create B.py

---
+++ B.py
@@ -0,0 +1,13 @@
+n=int(input())
+for i in range(n):
+    if i==0 or i==n-1 or i==n/2:
+        for j in range(0,int(n/2)-1):
+             print("* ",end =' ')
+        print()
+    else:
+        for j in range(int(n/2)):
+            if j==0 or j==int(n/2)-1:
+                print("* ",end =' ')
+            else:
+                print("  ",end =' ')
+        print() 
