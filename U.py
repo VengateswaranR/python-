dee8328d63bbcffcd82abce95dc28125bc311b37
VengateswaranR <97583836+VengateswaranR@users.noreply.github.com>
2022-02-03 14:17:55 +0530

    Create U.py

---
+++ U.py
@@ -0,0 +1,13 @@
+n=int(input())
+for i in range(n):
+    for j in range(n):
+        if i==n-1:
+            if j==0 or j==n-1:
+                print(" ",end=" ")
+            else:
+                print("*",end=" ")
+        elif i<n-1 and j==0 or j==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
