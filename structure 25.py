e3773defce74634bb6079d34011b42d8eb020801
VengateswaranR <97583836+VengateswaranR@users.noreply.github.com>
2022-02-09 12:28:40 +0530

    entrance 1

---
+++ structure 25.py
@@ -0,0 +1,8 @@
+n=int(input())
+for i in range (n):
+    for j in range(2*n+1):
+        if i%2==0 and (j>=n+i or j<=n-i) or j<=n-1-i or j>=n+1+i:
+            print("*",end=" ") 
+        else:
+            print(" ",end=" ")
+    print()
